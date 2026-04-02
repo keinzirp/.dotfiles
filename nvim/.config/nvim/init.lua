b74a882290719014f1e0fb1514d070e2a485a029
keinzirp <keinzirp@gmail.com>
2026-04-02 20:40:35 +1300

    feat: Add sql_formatter.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -455,6 +455,7 @@ require("lazy").setup({
 					typescript = { "biome-check", "biome-organize-imports" },
 					typescriptreact = { "biome-check", "biome-organize-imports" },
 					cpp = { "clang-format" },
+					sql = { "sql_formatter" },
 				},
 				format_on_save = function(bufnr)
 					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
